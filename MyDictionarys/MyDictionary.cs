29da757bb1841b53b29dfcd5cc0c79d8bd6fab15
kübra keskin <kubrakesskin@hotmail.com>
2021-01-22 21:00:34 +0300

    Add project files.

---
+++ MyDictionarys/MyDictionary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionarys
+{
+    class MyDictionary<T1, T2>
+    {
+        T1[] keys;
+        T2[] values;
+        T1[] tempKeys;
+        T2[] tempValues;
+
+        public MyDictionary()
+        {
+            keys = new T1[0];
+            values = new T2[0];
+
+        }
+        public void Add(T1 key1, T2 value1)
+        {
+            tempKeys = keys;
+            tempValues = values;
+            keys = new T1[keys.Length + 1];
+            values = new T2[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+            keys[keys.Length - 1] = key1;
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+
+        }
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+    
+    }
+
+}
---
+++ MyDictionarys/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionarys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> School = new MyDictionary<int, string>();
+
+            School.Add(1, "Süleyman Demirel Üniversitesi");
+            School.Add(2, "Akdeniz Üniversitesi");
+            School.Add(3, "Ankara Üniversitesi");
+            Console.WriteLine(School.Count);
+        }
+    }
+}
